71c2aa249b0d78d6698ad811f904f68bb4ec3698
sobuj <sobujahmed124@gmail.com>
2023-01-27 17:29:55 +0600

    array

---
+++ array.js
@@ -0,0 +1,16 @@
+// declaring arrays
+
+var friendsAge = [22, 23, 20, 21, 20, 22, 21, 20, 19]
+var picnicFee = [6000, 2000, 5000, 3000, 1000]
+var heroiens = ['sabnoor', 'purnima', 'sabana', 'sahara']
+
+// output of arrays
+console.log(friendsAge)
+console.log(picnicFee)
+console.log(heroiens)
+
+// finding length of array
+
+console.log(friendsAge.length)
+console.log(picnicFee.length)
+console.log(heroiens.length)
