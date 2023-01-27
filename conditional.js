90a346be27b25e84b4056d088ab752240fb3dd7e
sobuj <sobujahmed124@gmail.com>
2023-01-27 20:28:56 +0600

    conditional

---
+++ conditional.js
@@ -0,0 +1,18 @@
+// condition if else
+
+if (3 < 4) {
+  console.log('3 is smaller')
+} else {
+  console.log('4 is larger')
+}
+
+// with variables
+var foodPrice = 120
+var pocketMoney = 100
+
+// condition
+if (foodPrice < pocketMoney) {
+  console.log('i will eat food')
+} else {
+  console.log('i will not eat food')
+}
