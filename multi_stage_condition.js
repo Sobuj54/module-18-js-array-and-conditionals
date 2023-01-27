eac59cf781c91ef0188b02bfc7592a24d5ca98d5
sobuj <sobujahmed124@gmail.com>
2023-01-27 20:59:01 +0600

    multi stage condition

---
+++ multi_stage_condition.js
@@ -0,0 +1,14 @@
+var money = 10
+var toast = 90
+var biscuit = 60
+var samosa = 15
+
+if (toast < money) {
+  console.log('i will eat toast')
+} else if (biscuit < money) {
+  console.log('i will eat biscuit')
+} else if (samosa < money) {
+  console.log('i will eat samosa')
+} else {
+  console.log('i will eat singara')
+}
