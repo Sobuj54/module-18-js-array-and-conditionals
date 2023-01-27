35fec54b206376e6666c94ee1dab6b0903c659c3
sobuj <sobujahmed124@gmail.com>
2023-01-27 20:47:17 +0600

    multiple condition

---
+++ multiple_condition.js
@@ -0,0 +1,16 @@
+var isGraduated = true
+var salary = 25000
+var cars = 1
+// multiple conditions
+if (isGraduated == true && salary >= 30000 && cars >= 1) {
+  console.log('i will marry you')
+} else {
+  console.log('i will not marry you')
+}
+// another condion
+
+if (isGraduated == true || salary >= 30000 || cars >= 1) {
+  console.log('i will marry you')
+} else {
+  console.log('i will not marry you')
+}
